9f00598dc9e71afa7472b2b96cae85937c507c85
fedecampoos <40859084+fedecampoos@users.noreply.github.com>
2022-05-17 21:30:51 -0300

    fe
    hola

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proyectoinefrable.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
